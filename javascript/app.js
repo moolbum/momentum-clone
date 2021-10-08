bb32288d5e5afcb2a5f2f4ba77967fe8f3a689d4
moolbum <82882076+moolbum@users.noreply.github.com>
2021-10-09 02:59:42 +0900

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,28 @@
-alert('hi');
+const h1 = document.querySelector(".hello h1");
+
+function handleTitleClick(){
+    h1.style.color = "blue";
+}
+
+function handleMouseEnter(){
+    h1.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave(){
+    h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("nope!");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
